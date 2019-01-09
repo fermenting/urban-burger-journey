1d0dd83864938ad20f6af4789b6edd9e9cc5d739
Fermenting <realferalxanderyell@gmail.com>
2019-01-09 00:57:53 -0800

    laying foundation, also, heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,7 @@ INSERT INTO burgers (burger_name) VALUES ("Poutine on the Ritz");
 INSERT INTO burgers (burger_name) VALUES ("Beets of Burden");
 
 INSERT INTO burgers (burger_name) VALUES ("Run Gorgonzola Run Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Chorizo Your Own Adventure Burger");
+
+
---
+++ models/burger.js
@@ -0,0 +1,18 @@
+var orm = require("./config/orm.js");
+
+//code that will call the ORM functions using burger specific input for the ORM.
+
+
+
+
+
+
+
+// Find all the pets ordering by the lowest price to the highest price.
+orm.selectAndOrder("animal_name", "pets", "price");
+
+// Find a pet in the pets table by an animal_name of Rachel.
+orm.selectWhere("pets", "animal_name", "Rachel");
+
+// Find the buyer with the most pets.
+orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
